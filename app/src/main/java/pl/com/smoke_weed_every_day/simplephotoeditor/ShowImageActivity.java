b91b3f5d4aea5e727e04459f1372eed6886aa6b6
majalcmaj <majalcmaj@gmail.com>
2015-02-24 22:22:43 +0100

    Commit by Majalcmaj

--- app/src/main/java/pl/com/smoke_weed_every_day/simplephotoeditor/ShowImageActivity.java
+++ app/src/main/java/pl/com/smoke_weed_every_day/simplephotoeditor/ShowImageActivity.java
@@ -1,14 +1,38 @@
 package pl.com.smoke_weed_every_day.simplephotoeditor;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageView;
 
+import java.util.Arrays;
 public class ShowImageActivity extends Activity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_showimage);
-    }
+	private String path;
+	private Bitmap bitmap;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_showimage);
+		Intent intent = getIntent();
+		path = intent.getStringExtra("ImageToLoad");
+		try
+		{
+			bitmap = BitmapFactory.decodeFile(path);
+		}
+		catch(Exception exc)
+		{
+			Log.d("XD", "EKSCEPCJA");
+			throw new RuntimeException();
+		}
+		ImageView img = (ImageView) findViewById(R.id.bitmapViewer);
+		img.setImageBitmap(bitmap);
+	}
 }
