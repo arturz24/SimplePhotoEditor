2e837b9b1dc95d70278f0329515b4c0ad643f4db
majalcmaj <majalcmaj@gmail.com>
2015-02-24 19:26:20 +0100

    Beczkens

---
+++ app/src/main/java/pl/com/smoke_weed_every_day/simplephotoeditor/ShowImageActivity.java
@@ -0,0 +1,14 @@
+package pl.com.smoke_weed_every_day.simplephotoeditor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowImageActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showimage);
+    }
+}
